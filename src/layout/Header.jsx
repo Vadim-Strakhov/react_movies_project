98044625397f26406699b581f095745851ac8bd5
Vadim-Strakhov <mars1597@gmail.com>
2023-10-21 08:57:03 +0300

    refactoring the app

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
         <nav className='green darken-1'>
             <div className='nav-wrapper'>
                 <a
-                    href='#'
+                    href='https://github.com/Vadim-Strakhov/react_movies_project'
                     className='brand-logo'
                 >
                     React Movies
