199dd60c26bc6947afd4fc24f70297266e490f1f
Vadim-Strakhov <mars1597@gmail.com>
2023-11-12 14:42:55 +0300

    fixed jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,25 +1,24 @@
 import { Movie } from './Movie';
 
 export const Movies = (props) => {
-	const { movies = [] } = props;
+  const { movies = [] } = props;
 
-	return (
-		<div className='movies'>
-			{movies.length ? (
-				movies.map((movie) => {
-					return (
-						<Movie
-							key={movie.imdbID}
-							{...movie}
-						/>
-					);
-				})
-			) : (
-				<h4>
-					Упс, что-то пошло не так... <br />
-					Уже исправляю...
-				</h4>
-			)}
-		</div>
-	);
+  return (
+    <div className='movies'>
+      {movies.length ? (
+        movies.map((movie) => {
+          return (
+            <Movie
+              key={movie.imdbID}
+              {...movie}
+            />
+          );
+        })
+      ) : (
+        <div>
+          <h3>Not found</h3>
+        </div>
+      )}
+    </div>
+  );
 };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -6,44 +6,44 @@ import { Search } from '../components/Search';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const Main = () => {
-	const [movies, setMovies] = useState([]);
-	const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-	const searchMovies = (str, type = 'all') => {
-		setLoading(true);
-		fetch(
-			`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
-				type !== 'all' ? `&type=${type}` : ''
-			}`
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				setLoading(false);
-				setMovies(data.Search);
-			})
-			.catch((err) => {
-				console.log(err);
-				setLoading(false);
-			});
-	};
+  const searchMovies = (str, type = 'all') => {
+    setLoading(true);
+    fetch(
+      `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
+        type !== 'all' ? `&type=${type}` : ''
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setMovies(data.Search);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
 
-	useEffect(() => {
-		fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
-			.then((response) => response.json())
-			.then((data) => {
-				setMovies(data.Search);
-				setLoading(false);
-			})
-			.catch((err) => {
-				console.log(err);
-				setLoading(false);
-			});
-	}, []);
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=matrix`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
 
-	return (
-		<main className='container content'>
-			<Search searchMovies={searchMovies} />
-			{loading ? <Preloader /> : <Movies movies={movies} />}
-		</main>
-	);
+  return (
+    <main className='container content'>
+      <Search searchMovies={searchMovies} />
+      {loading ? <Preloader /> : <Movies movies={movies} />}
+    </main>
+  );
 };
